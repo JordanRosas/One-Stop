d21e1a16ea781b0f9426a2c129678bbca8e724e0
Jordan Rosas <jordanrosas1203@gmail.com>
2019-05-03 16:31:55 -0500

    working commit

--- OneStop/Models/JobTicket.cs
+++ OneStop/Models/JobTicket.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -11,6 +12,7 @@ namespace OneStop.Models
         public string UserId { get; set; }
         public int CompanyId { get; set; }
         public string Position { get; set; }
+        [Display(Name = "Date Created")]
         public string DateCreated { get; set; }
         public int StatusId { get; set; }
         public Status Status { get; set; }
