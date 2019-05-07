f02af7931e39a4d93418b82e27b0434059c1bf50
Jordan Rosas <jordanrosas1203@gmail.com>
2019-05-07 13:55:22 -0500

    added required data attributes

--- OneStop/Models/JobTicket.cs
+++ OneStop/Models/JobTicket.cs
@@ -10,10 +10,18 @@ namespace OneStop.Models
     {
         public int JobTicketId { get; set; }
         public string UserId { get; set; }
+
+        [Display(Name = "Select A Company")]
         public int CompanyId { get; set; }
+
+        [Required]
         public string Position { get; set; }
+
+        [Required]
         [Display(Name = "Todays Date")]
+        [DisplayFormat(DataFormatString = "{0:dd MMM yyyy}")]
         public DateTime DateCreated { get; set; }
+
         public int StatusId { get; set; }
         public Status Status { get; set; }
         public Company Company { get; set; }
